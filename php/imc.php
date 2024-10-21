f0b7c80d997578d946613af6e69a8223084e26f2
HugoDaniel1022 <soluciones.hdp@outlook.com>
2024-10-21 11:30:53 -0300

    m

--- php/imc.php
+++ php/imc.php
@@ -3,7 +3,44 @@
 if (count($_REQUEST) > 0) {
     $altura = $_REQUEST['altura'];
     $peso = $_REQUEST['peso'];
-    $altura_t = $altura/100;
 
-    echo "Su indice de masa corporal es: ".round($peso / ($altura_t**2), 2);
+    // Verificar si altura o peso son 0
+    if ($altura == 0 || $peso == 0) {
+        echo "Error: La altura o el peso no pueden ser cero.";
+    } else {
+        $altura_t = $altura / 100;
+        $resultado = round($peso / ($altura_t ** 2), 2);
+        $mensaje = "Su índice de masa corporal es: ";
+        $mensaje2 = ". Y su situación actual es ";
+        $mensajeFinal = $mensaje . $resultado . $mensaje2;
+
+        switch (true) {
+            case ($resultado > 0 && $resultado < 15):
+                echo $mensajeFinal . "delgadez muy severa";
+                break;
+            case ($resultado >= 15 && $resultado < 16):
+                echo $mensajeFinal . "delgadez severa";
+                break;
+            case ($resultado >= 16 && $resultado < 18.5):
+                echo $mensajeFinal . "delgadez";
+                break;
+            case ($resultado >= 18.5 && $resultado < 25):
+                echo $mensajeFinal . "normal";
+                break;
+            case ($resultado >= 25 && $resultado < 30):
+                echo $mensajeFinal . "sobrepeso";
+                break;
+            case ($resultado >= 30 && $resultado < 34.5):
+                echo $mensajeFinal . "obesidad moderada";
+                break;
+            case ($resultado >= 35 && $resultado < 40):
+                echo $mensajeFinal . "obesidad severa";
+                break;
+            case ($resultado >= 40):
+                echo $mensajeFinal . "obesidad mórbida";
+                break;
+        }
+    }
 }
+
+?>
